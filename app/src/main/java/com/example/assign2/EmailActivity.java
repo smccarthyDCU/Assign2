23cab1543362405e2e706260d279b0e8c40d22d8
Steven McCarthy <Steven.McCarthy56@mail.dcu.ie>
2023-12-22 22:21:29 +0000

    Updated the EmailActivity.java to include onClick

--- app/src/main/java/com/example/assign2/EmailActivity.java
+++ app/src/main/java/com/example/assign2/EmailActivity.java
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 //
@@ -21,13 +19,14 @@ public class EmailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email);
     }
-    public void save(View view) {
+    public void onClick(View view) {
         Intent intent = new Intent(this,MainActivity.class);
         EditText To = (EditText) findViewById(R.id.editTextText4);
         EditText Subject = (EditText) findViewById(R.id.editTextText3);
         EditText compose = (EditText) findViewById(R.id.textView7);
 
-
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
     }
 
 
